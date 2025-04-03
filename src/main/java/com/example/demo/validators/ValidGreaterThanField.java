4741ea2a53a50699d231b4f3f6fc90879c38af06
retupmoc258 <retupmoc258@gmail.com>
2025-04-02 18:38:11 -0600

    Makes @ValidGreaterThanField repeatable.

--- src/main/java/com/example/demo/validators/ValidGreaterThanField.java
+++ src/main/java/com/example/demo/validators/ValidGreaterThanField.java
@@ -2,14 +2,12 @@ package com.example.demo.validators;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = {GreaterThanValidator.class})
+@Repeatable(ValidGreaterThanFields.class)
 public @interface ValidGreaterThanField {
     String message() default "Field value must be more than the dependent value";
     Class<?>[] groups() default {};
@@ -18,3 +16,6 @@ public @interface ValidGreaterThanField {
     String dependentFieldName();
     boolean strictlyGreater() default true;
 }
+
+
+
